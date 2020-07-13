d2ca0203d67ed84128fe286c0d25c48c32c04b5e
Crowdbotics <team@crowdbotics.com>
2020-07-13 04:45:48 +0000

    Initial commit.

---
+++ web_13_dev_7481/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for web_13_dev_7481 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'web_13_dev_7481.settings')
+
+application = get_wsgi_application()
