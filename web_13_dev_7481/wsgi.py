ea27805f93c771549bc5cf0497d83bd27e024879
Crowdbotics <team@crowdbotics.com>
2020-07-13 06:08:57 +0000

    Update models

--- web_13_dev_7481/wsgi.py
+++ web_13_dev_7481/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'web_13_dev_7481.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "web_13_dev_7481.settings")
 
 application = get_wsgi_application()
